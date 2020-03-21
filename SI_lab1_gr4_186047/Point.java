b0fca829e47b179aa52391f09a3c166612015506
bbozilov26 <bbozilov26@users.noreply.github.com>
2020-03-21 23:12:52 +0100

    point class finished

--- SI_lab1_gr4_186047/Point.java
+++ SI_lab1_gr4_186047/Point.java
@@ -1,18 +1,76 @@
 class Point {
 	String id
 	double x,y;
+
 	//TODO add new variable
 	String color;
+
 	//TODO constructor
+    public Point(String id, double x, double y){
+        this.id = id;
+        this.x = x;
+        this.y = y;
+    }
 
 	//TODO setters and getters
+    public String getId() {
+        return id;
+    }
 
-	public void move (char xDirection, char yDirection) {
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void move (char xDirection, char yDirection) {
 		//TODO
+        if(xDirection == 'L' && yDirection == 'U'){
+            this.x = getX() - 1;
+            this.y = getY() + 1;
+        }
+
+        if(xDirection == 'L' && yDirection == 'D'){
+            this.x = getX() - 1;
+            this.y = getY() - 1;
+        }
+
+        if(xDirection == 'R' && yDirection == 'U'){
+            this.x = getX() + 1;
+            this.y = getY() + 1;
+        }
+
+        if(xDirection == 'R' && yDirection == 'D'){
+            this.x = getX() + 1;
+            this.y = getY() - 1;
+        }
 	}
 
 	public void draw () {
 		//TODO
+        System.out.println(this.id + "(" + this.x + ", " + this.y + ")");
 	}
 
 
