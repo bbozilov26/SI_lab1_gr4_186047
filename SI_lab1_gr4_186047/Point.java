f84825006ac317cf7b118484c778820efb8e80b8
bbozilov26 <bbozilov26@users.noreply.github.com>
2020-03-24 14:44:01 +0100

    point class finished

--- SI_lab1_gr4_186047/Point.java
+++ SI_lab1_gr4_186047/Point.java
@@ -73,33 +73,3 @@ class Point {
         System.out.println(this.id + "(" + this.x + ", " + this.y + ")");
 	}
 }
-
-class Canvas{
-    List<Point> pointList;
-
-    public List<Point> addPoint(Point point){
-        pointList.add(point);
-    }
-
-    public List<Point> removePoint(Point point){
-        pointList.remove(point);
-    }
-
-    public void findMiddlePoint(Point a, Point b){
-        double middlePointX = (a.x + b.x) / 2;
-        double middlePointY = (a.y + b.y) / 2;
-
-        System.out.println("Middle point = (" + middlePointX + ", "
-                + middlePointY + ")");
-    }
-
-    public moveAllPoints(){
-        char DirX, DirY;
-
-        for(Point p : pointList){
-            DirX = sc.nextLine();
-            DirY = sc.nextLine();
-            p.move(DirX, DirY);
-        }
-    }
-}
